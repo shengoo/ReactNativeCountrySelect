adfd149a72517538a6644c0b98bdcabd2180e646
shengoo <shengoo@hotmail.com>
2019-05-20 16:59:29 +0800

    update

--- App.js
+++ App.js
@@ -4,10 +4,6 @@ import {
   View,
   StyleSheet,
   SectionList,
-  FlatList,
-  ScrollView,
-  TouchableOpacity,
-  Vibration,
   SafeAreaView,
 } from 'react-native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
@@ -42,117 +38,113 @@ const sectionMapArr = [
   ['Z', 25],
 ];
 export default class App extends React.Component {
+
   state = {
     select: 'no',
   };
+
   constructor(props) {
     super(props);
     this.ps = [];
-    this.sectionlist = React.createRef();
     this.offsetY = 0;
+    this.sectionlist = React.createRef();
   }
+
   render() {
     const { select, key, show, showX, showY = -1000 } = this.state;
     return (
-      <SafeAreaView
-        style={styles.container}
-      >
-        <View
-          style={{ flex: 1, flexDirection: 'row' }}
-          onLayout={({
-            nativeEvent: {
-              layout: { x, y, width, height },
-            },
-          }) => {
+      <SafeAreaView style={styles.container}>
+        <SectionList
+          ref={this.sectionlist}
+          onLayout={({ nativeEvent: { layout: { y }, }, }) => {
             this.offsetY = y;
-          }}>
-          <View style={{ flex: 1, justifyContent: 'center' }}>
-            <SectionList
-              ref={this.sectionlist}
-              ItemSeparatorComponent={() => (<View style={{borderBottomColor: '#F8F8F8',borderBottomWidth: 1,}} />)}
-              renderItem={({ item, index, section }) => (
-                <View style={styles.itemContainer}>
-                  <Text style={styles.itemText} key={index}>
-                    {item.countryName}
-                  </Text>
-                </View>
-              )}
-              renderSectionHeader={({ section: { key } }) => (
-                <View style={styles.headerContainer}>
-                  <Text style={styles.headerText}>{key}</Text>
-                </View>
-              )}
-              sections={countries}
-              keyExtractor={(item, index) => item + index}
-            />
-          </View>
-          <View
-            style={{ width: 16, justifyContent: 'center' }}
-            onStartShouldSetResponder={evt => true}
-            onMoveShouldSetResponder={() => true}
-            onResponderGrant={evt => {
-              this.setState({
-                show: true,
-              });
-              // console.log('show');
-            }}
-            onResponderRelease={e => {
-              this.setState({
-                show: false,
-              });
-              // console.log('hide');
-            }}
-            onResponderReject={() => {
-              console.log('onResponderReject');
-            }}
-            onResponderMove={({ nativeEvent: { pageX, pageY, locationY } }) => {
-              const offsetY = pageY - this.offsetY;
-              const find = this.ps.find(
-                i => i.min < offsetY && i.max > offsetY
-              );
-              if (find) {
-                const { key } = this.state;
-                if (find.key !== key) {
-                  this.setState({
-                    key: find.key,
-                    showX: pageX,
-                    showY: find.min,
-                  });
-                  this.sectionlist.current.scrollToLocation({
-                    sectionIndex: find.index,
-                    itemIndex: 0,
-                    animated: false,
-                  });
-                  ReactNativeHapticFeedback.trigger('impactLight', {
-                    enableVibrateFallback: true,
-                    ignoreAndroidSystemSettings: false,
-                  });
-                }
+          }}
+          onScrollToIndexFailed={() => {
+
+          }}
+          containerStyle={{ flex: 1, justifyContent: 'center' }}
+          ItemSeparatorComponent={() => (<View style={{ borderBottomColor: '#F8F8F8', borderBottomWidth: 1, }} />)}
+          renderItem={({ item, index, section }) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemText} key={index}>
+                {item.countryName}
+              </Text>
+            </View>
+          )}
+          renderSectionHeader={({ section: { key } }) => (
+            <View style={styles.headerContainer}>
+              <Text style={styles.headerText}>{key}</Text>
+            </View>
+          )}
+          sections={countries}
+          keyExtractor={(item, index) => item + index}
+        />
+        <View
+          style={{ width: 16, justifyContent: 'center', alignItems: 'center' }}
+          onStartShouldSetResponder={() => true}
+          onMoveShouldSetResponder={() => true}
+          onResponderGrant={evt => {
+            this.setState({
+              show: true,
+            });
+          }}
+          onResponderRelease={e => {
+            this.setState({
+              show: false,
+            });
+          }}
+          onResponderReject={() => {
+            console.log('onResponderReject');
+          }}
+          onResponderMove={({ nativeEvent: { pageX, pageY } }) => {
+            const offsetY = pageY - this.offsetY;
+            const find = this.ps.find(
+              i => i.min < offsetY && i.max > offsetY
+            );
+            if (find) {
+              const { key } = this.state;
+              if (find.key !== key) {
+                this.setState({
+                  key: find.key,
+                  showX: pageX,
+                  showY: find.min,
+                });
+                this.sectionlist.current.scrollToLocation({
+                  sectionIndex: find.index,
+                  itemIndex: 0,
+                  animated: false,
+                });
+                ReactNativeHapticFeedback.trigger('impactLight', {
+                  enableVibrateFallback: true,
+                  ignoreAndroidSystemSettings: false,
+                });
               }
-            }}>
-            {sectionMapArr.map((item, index) => {
-              return (
-                <Text
-                  key={index}
-                  onLayout={({
-                    nativeEvent: {
-                      layout: { x, y, width, height },
-                    },
-                  }) => {
-                    // console.log(item[0], x, y, width, height);
-                    this.ps = this.ps.filter(i => i.key !== item[0]);
-                    this.ps.push({
-                      key: item[0],
-                      min: y,
-                      max: y + height,
-                      index: item[1],
-                    });
-                  }}>
-                  {item[0]}
-                </Text>
-              );
-            })}
-            {show && (
+            }
+          }}
+        >
+          {sectionMapArr.map((item, index) => {
+            return (
+              <Text
+                key={index}
+                onLayout={({
+                  nativeEvent: {
+                    layout: { x, y, width, height },
+                  },
+                }) => {
+                  this.ps = this.ps.filter(i => i.key !== item[0]);
+                  this.ps.push({
+                    key: item[0],     // 对应的字母 A-Z
+                    min: y,           // 字母顶部Y坐标
+                    max: y + height,  // 字母底部Y坐标
+                    index: item[1],   // 字母对应SectionList的index
+                  });
+                }}
+              >
+                {item[0]}
+              </Text>
+            );
+          })}
+          {show && (
               <View style={{
                 position: 'absolute', left: -60, top: showY - 10,borderRadius: 20,width: 60, height: 40,
                 flexDirection: 'row',
@@ -185,7 +177,6 @@ export default class App extends React.Component {
                 </View>
               </View>
             )}
-          </View>
         </View>
       </SafeAreaView>
     );
@@ -195,11 +186,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: '#FFFFFF',
-    // padding: 8,
-    // flexDirection: 'row',
+    flexDirection: 'row',
   },
   headerContainer: {
     padding: 5,
